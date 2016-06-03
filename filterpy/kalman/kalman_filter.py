5b0b320faccbffa0be0eacd5d9e6211e2e0598fe
Vincent Schut <schut@satelligence.com>
2016-06-03 16:59:28 +0200

    does not work. Try to always cast to array.

--- filterpy/kalman/kalman_filter.py
+++ filterpy/kalman/kalman_filter.py
@@ -954,8 +954,7 @@ def predict(x, P, F=1, Q=0, u=0, B=1, alpha=1.):
     """
 
     #if np.isscalar(F):
-    if not hasattr(F, 'dtype'):
-        F = np.array(F)
+    F = np.array(F)
     x = dot(F, x) + dot(B, u)
     P = (alpha * alpha) * dot3(F, P, F.T) + Q
 
