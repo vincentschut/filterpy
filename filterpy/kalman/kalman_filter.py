5093510f2a48c3528e39829a9932389d16534dc0
Vincent Schut <schut@satelligence.com>
2016-06-06 13:03:52 +0200

    numba compatibility

--- filterpy/kalman/kalman_filter.py
+++ filterpy/kalman/kalman_filter.py
@@ -955,6 +955,7 @@ def predict(x, P, F=1, Q=0, u=0, B=1, alpha=1.):
 
     #if np.isscalar(F):
     F = np.array(F)
+    print("PREDICT:", F, x, B, u)
     x = dot(F, x) + dot(B, u)
     P = (alpha * alpha) * dot3(F, P, F.T) + Q
 
